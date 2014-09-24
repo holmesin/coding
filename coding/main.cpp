a693a4ccfb4d2de67f42a2608d3ea80f24bfaabd
holmesin <holmes008@live.cn>
2014-09-23 23:12:24 -0700

    Initial Commit

---
+++ coding/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  coding
+//
+//  Created by Sherlock Holmes on 2014-09-23.
+//  Copyright (c) 2014 Sherlock Holmes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
